e8391d0b8c412ce44a2329e033699b2fbf9db98b
leoxu0257 <enmacorradi@163.com>
2021-05-21 18:47:25 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var small = document.getElementById('small');
+var medium = document.getElementById('medium');
+var large = document.getElementById('large');
+var submitBtn = document.getElementById('submit');
+var size = document.getElementById('size-detail');
+var cartNumber = document.getElementById('cart-number');
+var sizeSelected = 0;
+var total = 0;
+
+small.onclick = function() {
+    size.innerHTML = "S";
+    sizeSelected = 1;
+};
+
+medium.onclick = function() {
+    size.innerHTML = "M";
+    sizeSelected = 1;
+};
+
+large.onclick = function() {
+    size.innerHTML = "L";
+    sizeSelected = 1;
+};
+
+submitBtn.onclick = function() {
+    total++;
+    cartNumber.innerHTML = total;
+};
